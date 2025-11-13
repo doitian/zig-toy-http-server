89eae5c1f7aa27a4b6aec9e3f93154722b149cd1
ian <me@iany.me>
2025-11-13 22:13:07 +0800

    experiment on std net

--- src/main.zig
+++ src/main.zig
@@ -1,8 +1,22 @@
-const zig_toy_http_server = @import("zig_toy_http_server");
+const std = @import("std");
 
-const Handler = struct {};
+const http = @import("zig_toy_http_server");
+
+const Context = struct {};
+
+fn handle(context: *Context, req: *http.Request, res: *http.Response) anyerror!void {
+    _ = context;
+    _ = res;
+    var buffer: [4096]u8 = undefined;
+    @memset(buffer[0..buffer.len], 0);
+    const readLen = try req.reader.readSliceShort(buffer[0..buffer.len]);
+    std.debug.print("{s}", .{buffer[0..readLen]});
+}
 
 pub fn main() !void {
-    _ = zig_toy_http_server.Server(Handler).init(.{});
-    // _ = server;
+    var general_purpose_allocator: std.heap.GeneralPurposeAllocator(.{}) = .init;
+    const gpa = general_purpose_allocator.allocator();
+
+    var server = http.Server(Context).init(.{}, handle);
+    try server.listenAndServe(gpa, "127.0.0.1:80");
 }
--- src/root.zig
+++ src/root.zig
@@ -1,22 +1,47 @@
 const std = @import("std");
+const net = std.net;
 
-pub const Request = struct {};
+pub const Request = struct {
+    reader: *std.Io.Reader,
+};
 
 pub const Response = struct {};
 
-pub fn Server(comptime Handler: type) type {
-    if (@typeInfo(Handler) != .@"struct") {
-        @compileError("Server handler must be a struct, got: " ++ @tagName(@typeInfo(Handler)));
+pub fn Server(comptime Context: type) type {
+    if (@typeInfo(Context) != .@"struct") {
+        @compileError("expected struct argument, found " ++ @typeName(Context));
     }
     return struct {
-        handler: Handler,
-
+        const Handler = *const fn (context: *Context, req: *Request, res: *Response) anyerror!void;
         const Self = @This();
 
-        pub fn init(handler: Handler) Self {
+        context: Context,
+        handle: Handler,
+
+        pub fn init(context: Context, handle: Handler) Self {
             return .{
-                .handler = handler,
+                .context = context,
+                .handle = handle,
             };
+        }
+
+        pub fn listenAndServe(self: *Self, allocator: std.mem.Allocator, address: []const u8) !void {
+            var parsed_address = try net.Address.parseIpAndPort(address);
+            // Use 80 as default port
+            if (parsed_address.getPort() == 0) {
+                parsed_address.setPort(80);
+            }
+            var server = try parsed_address.listen(.{});
+            std.debug.print("Listening on {f}\n", .{parsed_address});
+            while (true) {
+                const conn = try server.accept();
+                const buffer = try allocator.alloc(u8, 4096);
+                defer allocator.free(buffer);
+                var reader = conn.stream.reader(buffer);
+                var req = Request{ .reader = reader.interface() };
+                var resp = Response{};
+                try self.handle(&self.context, &req, &resp);
+            }
         }
     };
 }
